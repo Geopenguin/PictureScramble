9becb8b60ec17d411a5dd96bc8893d22d0db3f85
pedromendezjr44@gmail.com <pedromendezjr44@gmail.com>
2022-11-13 04:26:58 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace PictureScramble
+{
+    partial class uxScrambleMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(uxScrambleMain));
+            this.uxImageGetter = new System.Windows.Forms.OpenFileDialog();
+            this.uxGenerator = new System.Windows.Forms.Button();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.uxImageHolder = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.uxImageHolder.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // uxImageGetter
+            // 
+            this.uxImageGetter.FileName = "\"\"";
+            this.uxImageGetter.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // uxGenerator
+            // 
+            this.uxGenerator.Location = new System.Drawing.Point(6, 3);
+            this.uxGenerator.Name = "uxGenerator";
+            this.uxGenerator.Size = new System.Drawing.Size(440, 23);
+            this.uxGenerator.TabIndex = 0;
+            this.uxGenerator.Text = "Select Image ";
+            this.uxGenerator.UseVisualStyleBackColor = true;
+            this.uxGenerator.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(291, 247);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(138, 116);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(147, 247);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(138, 116);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(3, 247);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(138, 116);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(291, 125);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(138, 116);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(147, 125);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(138, 116);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(3, 125);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(138, 116);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.InitialImage = null;
+            this.pictureBox3.Location = new System.Drawing.Point(291, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(138, 116);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(147, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(138, 116);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(138, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // uxImageHolder
+            // 
+            this.uxImageHolder.Controls.Add(this.pictureBox1);
+            this.uxImageHolder.Controls.Add(this.pictureBox2);
+            this.uxImageHolder.Controls.Add(this.pictureBox3);
+            this.uxImageHolder.Controls.Add(this.pictureBox4);
+            this.uxImageHolder.Controls.Add(this.pictureBox5);
+            this.uxImageHolder.Controls.Add(this.pictureBox6);
+            this.uxImageHolder.Controls.Add(this.pictureBox7);
+            this.uxImageHolder.Controls.Add(this.pictureBox8);
+            this.uxImageHolder.Controls.Add(this.pictureBox9);
+            this.uxImageHolder.Location = new System.Drawing.Point(7, 32);
+            this.uxImageHolder.MaximumSize = new System.Drawing.Size(559, 407);
+            this.uxImageHolder.Name = "uxImageHolder";
+            this.uxImageHolder.Size = new System.Drawing.Size(439, 379);
+            this.uxImageHolder.TabIndex = 1;
+            // 
+            // uxScrambleMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(448, 415);
+            this.Controls.Add(this.uxImageHolder);
+            this.Controls.Add(this.uxGenerator);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(464, 454);
+            this.Name = "uxScrambleMain";
+            this.Text = "PictureScramble";
+            this.Load += new System.EventHandler(this.uxScrambleMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.uxImageHolder.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog uxImageGetter;
+        private System.Windows.Forms.Button uxGenerator;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel uxImageHolder;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PictureScramble
+{
+    public partial class uxScrambleMain : Form
+    {
+        /// <summary>
+        /// Field for num of boxes
+        /// </summary>
+        private const int _boxes = 9;
+
+        /// <summary>
+        /// Private field to hold box info 
+        /// </summary>
+        private PictureBox[] p = new PictureBox[_boxes];
+
+        /// <summary>
+        /// Init Gui 
+        /// </summary>
+        public uxScrambleMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Main GUI 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxScrambleMain_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Tag = 1;
+            pictureBox2.Tag = 2;
+            pictureBox3.Tag = 3;
+            pictureBox4.Tag = 4;
+            pictureBox5.Tag = 5;
+            pictureBox6.Tag = 6;
+            pictureBox7.Tag = 7;
+            pictureBox8.Tag = 8;
+            pictureBox9.Tag = 9;
+
+            int i = 0;
+            foreach (PictureBox pic in uxImageHolder.Controls) //init array with tag info 
+            {
+                p.SetValue(pic, i);
+                i++;
+            }
+        
+        }
+
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Event Handler for generating image 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (uxImageGetter.ShowDialog() == DialogResult.OK)
+            {
+
+                Image[] imageArray = new Image[_boxes]; //init image array 
+                Image img = Image.FromFile(uxImageGetter.FileName); //gets image from user input 
+                int widthThird = (int)((double)img.Width / 3.0 + 0.5); //proportions wdith
+                int heightThird = (int)((double)img.Height / 3.0 + 0.5); //proportions height 
+                Bitmap[,] bmps = new Bitmap[3, 3]; //init bitmap
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++) //iterates 9 times to set each image 
+                    {
+                        bmps[i, j] = new Bitmap(widthThird, heightThird); //image at coordinate will have these dimensions
+                        Graphics g = Graphics.FromImage(bmps[i, j]); //init g to specified bitmap 
+                        g.DrawImage(img, new Rectangle(0, 0, widthThird, heightThird), new Rectangle(j * widthThird, i * heightThird, widthThird, heightThird), GraphicsUnit.Pixel); //draws image out 
+                        g.Dispose(); //dispose resources 
+                    }
+
+                }
+                pictureBox1.Image = bmps[0, 0];
+                pictureBox2.Image = bmps[0, 1];
+                pictureBox3.Image = bmps[0, 2];
+                pictureBox4.Image = bmps[1, 0];
+                pictureBox5.Image = bmps[1, 1];
+                pictureBox6.Image = bmps[1, 2];
+                pictureBox7.Image = bmps[2, 0];
+                pictureBox8.Image = bmps[2, 1];
+            }
+        }
+
+        /// <summary>
+        /// Method to swap two Pictureboxes 
+        /// </summary>
+        /// <param name="one"></param>
+        /// <param name="two"></param>
+        public void Swap(PictureBox one, PictureBox two)
+        {
+
+            int indexAlpha = uxImageHolder.Controls.IndexOf(one);
+            int indexBeta = uxImageHolder.Controls.IndexOf(two);
+
+            uxImageHolder.Controls.SetChildIndex(one, indexBeta);
+            uxImageHolder.Controls.SetChildIndex(two, indexAlpha);
+
+        }
+
+        /// <summary>
+        /// Checks and compares tag info with other pictureboxes
+        /// </summary>
+        /// <param name="current"></param>
+        public bool CheckThenSwap(PictureBox current)
+        {
+            for (int i = 0; i < _boxes ; i++)
+            {
+                if (p[i].Tag.Equals(9) && current.Tag != null)
+                {
+                    Swap(p[i], current);
+                }
+            }
+            return false; 
+        }
+        /// <summary>
+        /// Picture 1 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+            CheckThenSwap(pictureBox1);
+        }
+
+        /// <summary>
+        /// Picture box 2
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            CheckThenSwap(pictureBox2);
+        }
+
+        /// <summary>
+        /// Picture box 3
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            CheckThenSwap(pictureBox3);
+        }
+
+        /// <summary>
+        /// Picture box 4
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            CheckThenSwap(pictureBox4);
+        }
+
+        /// <summary>
+        /// Picture box 8
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            CheckThenSwap(pictureBox8);
+        }
+
+        /// <summary>
+        /// Picture box 5
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            CheckThenSwap(pictureBox5);
+        }
+
+        /// <summary>
+        /// Picture box 6
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            CheckThenSwap(pictureBox6);
+        }
+
+        /// <summary>
+        /// Picture box 7
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            CheckThenSwap(pictureBox7);
+        }
+
+        /// <summary>
+        /// Empty Picture box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
